38271b422cd10178c39344bb42cedb6a68e3f0f6
GaeNari <gaenari31@gmail.com>
2015-04-20 19:49:02 +0900

    db, additional test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -21,13 +21,39 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test '#signup_failure_dup' do
-    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
-    json = JSON.parse(response.body)
-    assert json['user_name'] == 'testuser'
-    assert json['login_count'] == 1
-
-    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    post :signup, {'username' => 'Romeo', 'password' => 'ihatejuliet'}
     json = JSON.parse(response.body)
     assert json['error_code'] == -3
+  end
+
+  test '#login_success' do
+    post :login, {'username' => 'Romeo', 'password' => 'ilovejuliet'}
+    json = JSON.parse(response.body)
+    assert json['user_name'] == 'Romeo'
+    assert json['login_count'] == 101
+  end
+
+  test '#login_failure' do
+    post :login, {'username' => 'Shrek', 'password' => 'stupidfiona'}
+    json = JSON.parse(response.body)
+    assert json['error_code'] == -4
+  end
+
+  test '#logout' do
+    get :logout
+    #assert_redirect_to root_path
+  end
+
+  test '#clearData' do
+    post :clear_data
+    post :login, {'username' => 'Romeo', 'password' => 'ilovejuliet'}
+    json = JSON.parse(response.body)
+    assert json['error_code'] == -4
+  end
+
+  test '#user_count' do
+    post :user_count
+    json = JSON.parse(response.body)
+    assert json['user_count'] == 2
   end
 end
